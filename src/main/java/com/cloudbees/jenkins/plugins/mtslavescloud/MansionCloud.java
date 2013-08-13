842fecfda4260d2f73e7d8d3a06b13d3e1e67364
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-08-12 18:40:54 -0700

    Talk to GC and generate a proper token, as opposed to a hard-coded value

--- src/main/java/com/cloudbees/jenkins/plugins/mtslavescloud/MansionCloud.java
+++ src/main/java/com/cloudbees/jenkins/plugins/mtslavescloud/MansionCloud.java
@@ -1,5 +1,10 @@
 package com.cloudbees.jenkins.plugins.mtslavescloud;
 
+import com.cloudbees.EndPoints;
+import com.cloudbees.api.BeesClient;
+import com.cloudbees.api.cr.Capability;
+import com.cloudbees.api.oauth.OauthClientException;
+import com.cloudbees.api.oauth.TokenRequest;
 import com.cloudbees.jenkins.plugins.sshcredentials.SSHUser;
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey;
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey.DirectEntryPrivateKeySource;
@@ -11,8 +16,12 @@ import com.cloudbees.mtslaves.client.VirtualMachineConfigurationException;
 import com.cloudbees.mtslaves.client.VirtualMachineRef;
 import com.cloudbees.mtslaves.client.VirtualMachineSpec;
 import com.cloudbees.mtslaves.client.properties.SshdEndpointProperty;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.cloudbees.CloudBeesAccount;
+import com.cloudbees.plugins.credentials.cloudbees.CloudBeesUser;
 import com.trilead.ssh2.signature.RSAPublicKey;
 import com.trilead.ssh2.signature.RSASHA1Verify;
+import hudson.AbortException;
 import hudson.CopyOnWrite;
 import hudson.Extension;
 import hudson.Util;
@@ -25,8 +34,9 @@ import hudson.slaves.AbstractCloudImpl;
 import hudson.slaves.Cloud;
 import hudson.slaves.NodeProvisioner.PlannedNode;
 import hudson.util.DescribableList;
+import hudson.util.ListBoxModel;
+import hudson.util.Secret;
 import jenkins.model.Jenkins;
-import net.sf.json.JSONObject;
 import org.bouncycastle.openssl.PEMWriter;
 import org.jenkinsci.main.modules.instance_identity.InstanceIdentity;
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -38,12 +48,12 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static java.util.logging.Level.*;
 
 /**
  * {@link Cloud} implementation that talks to CloudBees' multi-tenant slaves.
@@ -52,6 +62,12 @@ import java.util.logging.Logger;
  */
 public class MansionCloud extends AbstractCloudImpl {
     private final URL broker;
+
+    /**
+     * A {@link CloudBeesUser} can have access to multiple accounts,
+     * so this field would specify which account is used to request slaves.
+     */
+    private String account;
 
     public SnapshotRef lastSnapshot = null;
 
@@ -63,9 +79,10 @@ public class MansionCloud extends AbstractCloudImpl {
             = new DescribableList<MansionCloudProperty,MansionCloudPropertyDescriptor>(Jenkins.getInstance());
 
     @DataBoundConstructor
-    public MansionCloud(URL broker, List<MansionCloudProperty> properties) throws IOException {
+    public MansionCloud(URL broker, String account, List<MansionCloudProperty> properties) throws IOException {
         super("mansion"+ Util.getDigestOf(broker.toExternalForm()).substring(0,8), "0"/*unused*/);
         this.broker = broker;
+        this.account = account;
         if (properties!=null)
             this.properties.replaceBy(properties);
     }
@@ -79,6 +96,10 @@ public class MansionCloud extends AbstractCloudImpl {
      */
     public URL getBroker() {
         return broker;
+    }
+
+    public String getAccount() {
+        return account;
     }
 
     @Override
@@ -106,6 +127,24 @@ public class MansionCloud extends AbstractCloudImpl {
         }
     }
 
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl)super.getDescriptor();
+    }
+
+    public String createAccessToken(URL broker) throws AbortException, OauthClientException {
+        CloudBeesUser u = getDescriptor().findUser();
+        BeesClient bees = new BeesClient(EndPoints.runAPI(),u.getAPIKey(), Secret.toString(u.getAPISecret()), null, null);
+
+        CloudBeesAccount acc = u.getAccount(Util.fixNull(account));
+        if (acc==null)      acc = u.getAccounts().get(0); // fallback
+
+        TokenRequest tr = new TokenRequest()
+            .withAccountName(acc.getName())
+            .withScope(broker, PROVISION_CAPABILITY)
+            .withGenerateRequestToken(false);
+        return bees.getOauthClient().createToken(tr).accessToken;
+    }
 
     @Override
     public Collection<PlannedNode> provision(Label label, int excessWorkload) {
@@ -136,22 +175,22 @@ public class MansionCloud extends AbstractCloudImpl {
                 spec.sshd(JENKINS_USER, 15000, publicKey.trim()); // TODO: should UID be configurable?
                 final VirtualMachineRef vm;
 
-    		    //TODO : allow user to configure oauth token from credentials plugin
-                String oauthToken = "88e7313d64af5ee654525625885be2781eb9bae0";
                 HardwareSpec box = new HardwareSpec("small");
 
                 URL broker = new URL(this.broker,"/"+st.mansion+"/");
-                if (MANSION_SECRET==null)
+                if (MANSION_SECRET==null) {
+                    String oauthToken = createAccessToken(broker);
                     vm = new BrokerRef(broker).createVirtualMachine(box, oauthToken);
-                else
-                    vm = new MansionRef(broker,MANSION_SECRET).createVirtualMachine(box, oauthToken);
+                } else {
+                    vm = new MansionRef(broker,MANSION_SECRET).createVirtualMachine(box, "unused");
+                }
 
                 LOGGER.fine("Allocated "+vm.url);
                 try {
                     vm.setup(spec);
                 } catch (VirtualMachineConfigurationException e) {
                     //TODO: retry with another snapshot
-                    LOGGER.log(Level.WARNING, "Failed to configure VM",e);
+                    LOGGER.log(WARNING, "Failed to configure VM",e);
                 }
 
                 Future<Node> f = Computer.threadPoolForRemoting.submit(new Callable<Node>() {
@@ -196,9 +235,11 @@ public class MansionCloud extends AbstractCloudImpl {
                 r.add(new PlannedNode(vm.getId(),f,1));
             }
         } catch (IOException e) {
-            LOGGER.log(Level.WARNING, "Failed to provision from "+this,e);
+            LOGGER.log(WARNING, "Failed to provision from "+this,e);
         } catch (InterruptedException e) {
-            LOGGER.log(Level.WARNING, "Failed to provision from " + this, e);
+            LOGGER.log(WARNING, "Failed to provision from " + this, e);
+        } catch (OauthClientException e) {
+            LOGGER.log(WARNING, "Failed to provision from " + this, e);
         }
         return r;
     }
@@ -232,6 +273,23 @@ public class MansionCloud extends AbstractCloudImpl {
         public String getDisplayName() {
             return "Multi-tenancy Slave Cloud";
         }
+
+        private CloudBeesUser findUser() throws AbortException {
+            // TODO: perhaps we should also let the user configure which credential to use?
+            for (CloudBeesUser user : CredentialsProvider.lookupCredentials(CloudBeesUser.class)) {
+                return user;
+            }
+            throw new AbortException("No cloudbees account is registered with this Jenkins instance.");
+        }
+
+        public ListBoxModel doFillAccountItems() throws AbortException {
+            CloudBeesUser user = findUser();
+            ListBoxModel r = new ListBoxModel();
+            for (CloudBeesAccount acc : user.getAccounts()) {
+                r.add(acc.getDisplayName()+" ("+acc.getName()+")", acc.getName());
+            }
+            return r;
+        }
     }
 
     private static final Logger LOGGER = Logger.getLogger(MansionCloud.class.getName());
@@ -246,4 +304,14 @@ public class MansionCloud extends AbstractCloudImpl {
      * a mansion without going through a broker. Useful during development.
      */
     public static String MANSION_SECRET = System.getProperty("mansion.secret");
+
+    // TODO: move to the mt-slaves-client
+    public static Capability PROVISION_CAPABILITY = new Capability("https://types.cloudbees.com/broker/provision");
+
+    /**
+     * Are we running inside DEV@cloud?
+     */
+    public static boolean isInDevAtCloud() {
+        return Jenkins.getInstance().getPlugin("cloudbees-account")!=null;
+    }
 }
--- src/main/resources/com/cloudbees/jenkins/plugins/mtslavescloud/MansionCloud/config.groovy
+++ src/main/resources/com/cloudbees/jenkins/plugins/mtslavescloud/MansionCloud/config.groovy
@@ -1,11 +1,18 @@
 package com.cloudbees.jenkins.plugins.mtslavescloud.MansionCloud
 
+import com.cloudbees.jenkins.plugins.mtslavescloud.MansionCloud
 import com.cloudbees.jenkins.plugins.mtslavescloud.MansionCloudPropertyDescriptor;
 
 def f = namespace(lib.FormTagLib)
 
 f.entry(field:"broker",title:_("Broker URL")) {
     f.textbox()
+}
+
+if (!MansionCloud.isInDevAtCloud()) {// in DEV@cloud, the implicit user credential only has one account
+    f.entry(field:"account",title:_("Account")) {
+        f.select()
+    }
 }
 
 f.descriptorList(field:"properties",  title:_("Properties"), descriptors:MansionCloudPropertyDescriptor.all())
