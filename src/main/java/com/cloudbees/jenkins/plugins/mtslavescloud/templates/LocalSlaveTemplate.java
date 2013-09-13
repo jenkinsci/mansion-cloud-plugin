c064e7023d50c6fc03004ac9617e84826f36e6df
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-09-13 15:34:48 -0700

    Made persistent file systems customizable

--- src/main/java/com/cloudbees/jenkins/plugins/mtslavescloud/templates/LocalSlaveTemplate.java
+++ src/main/java/com/cloudbees/jenkins/plugins/mtslavescloud/templates/LocalSlaveTemplate.java
@@ -6,6 +6,8 @@ import hudson.model.Descriptor.FormException;
 import net.sf.json.JSONObject;
 
 import javax.servlet.ServletException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * {@link SlaveTemplate} that lets the user defines the spec JSON. Experimental.
@@ -15,6 +17,10 @@ import javax.servlet.ServletException;
 public class LocalSlaveTemplate extends SlaveTemplate {
     private String mansionType;
     private String definition;
+    /**
+     * List of persistent file systems separated by NL.
+     */
+    private String persistentFileSystems;
 
     public LocalSlaveTemplate(String displayName) {
         super(displayName);
@@ -34,11 +40,21 @@ public class LocalSlaveTemplate extends SlaveTemplate {
         return mansionType;
     }
 
+    public String getPersistentFileSystemsStr() {
+        return persistentFileSystems;
+    }
+
+    @Override
+    public List<String> getPersistentFileSystems() {
+        return Arrays.asList(persistentFileSystems.split("[\r\n]+"));
+    }
+
     @Override
     protected void submit(JSONObject json) throws ServletException, FormException {
         super.submit(json);
         definition = json.getString("definition");
         mansionType = json.getString("mansionType");
+        persistentFileSystems = Util.fixEmptyAndTrim(json.getString("persistentFileSystemsStr"));
     }
 
     @Extension
